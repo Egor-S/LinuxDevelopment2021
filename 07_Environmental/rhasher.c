a5b9d5c303c63d77aa5a6ea3654d486a66c7fd17
Egor Sklyarov <egor.sklyarov.ru@gmail.com>
2021-11-05 19:41:37 +0300

    07: rhash & readline

--- 07_Environmental/rhasher.c
+++ 07_Environmental/rhasher.c
@@ -1,14 +1,88 @@
 #include <stdio.h>
-// #include <rhash.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <errno.h>
+#include <config.h>
+#include <rhash.h>
+
+#ifdef HAVE_LIBREADLINE
+#include <readline/readline.h>
+#include <readline/history.h>
+#endif 
 
 
-#define MAX_LINE_SIZE 1024
+int strcmpi(char const *a, char const *b) {
+	/* source: https://stackoverflow.com/questions/5820810 */
+    for (;; a++, b++) {
+        int d = tolower((unsigned char)*a) - tolower((unsigned char)*b);
+        if (d != 0 || !*a)
+            return d;
+    }
+}
+
+
+enum rhash_ids get_hash_id(char const *hash_name) {
+	if (strcmpi(hash_name, "TTH") == 0)  return RHASH_TTH;
+	if (strcmpi(hash_name, "SHA1") == 0) return RHASH_SHA1;
+	if (strcmpi(hash_name, "MD5") == 0)  return RHASH_MD5;
+	return 0;
+}
 
 
 int main(int argc, char *argv[]) {
-	char line[MAX_LINE_SIZE];
-	while (fgets(line, MAX_LINE_SIZE, stdin)) {
-		printf("hi\n");
+	char *line = NULL;
+	
+	char digest[64];
+    char output[130];
+    rhash_library_init();
+
+    #ifndef HAVE_LIBREADLINE
+	size_t len = 0;
+	ssize_t n_read;
+	while ((n_read = getline(&line, &len, stdin)) != -1) {
+		line[len - 1] = '\0';  // trim \n
+	#else
+	while ((line = readline(NULL))) {
+		if (*line)
+			add_history(line);
+	#endif
+		char *hash_name;
+		char *payload;
+		if ((hash_name = strtok(line, " ")) == NULL || (payload = strtok(NULL, " ")) == NULL) {
+			fprintf(stderr, "Not enough arguments. Usage: <hash> <\"string|file>\n");
+			continue;
+		}
+		if (strtok(NULL, " ") != NULL) {
+			fprintf(stderr, "Too many arguments. Usage: <hash> <\"string|file>\n");
+			continue;
+		}
+		int hash_id = get_hash_id(hash_name);
+		if (!hash_id) {
+			fprintf(stderr, "Unknown hash name: %s\n", hash_name);
+			continue;
+		}
+		unsigned char c = hash_name[0];
+		int format = (c == tolower(c)) ? RHPR_BASE64 : RHPR_HEX;
+
+		if (payload[0] == '"') {
+			payload++;  // trim quote
+			int res = rhash_msg(hash_id, payload, strlen(payload), digest);
+			if (res < 0) {
+				fprintf(stderr, "message digest calculation error\n");
+				continue;
+			}
+		} else {
+			int res = rhash_file(hash_id, payload, digest);
+			if(res < 0) {
+				fprintf(stderr, "LibRHash error: %s: %s\n", payload, strerror(errno));
+				continue;
+			}
+		}
+		rhash_print_bytes(output, digest, rhash_get_digest_size(hash_id), (format | RHPR_UPPERCASE));
+		printf("%s\n", output);
 	}
+	
+	free(line);
 	return 0;
 }
